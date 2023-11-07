c1d66ea5fa82e3641ae9791fddec75d56dff3b83
WhoIsIt <147487664+WhoIsIt619@users.noreply.github.com>
2023-11-07 09:24:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/laba9/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.laba9;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ProgressBar indicatorBar = findViewById(R.id.indicator);
+        TextView statusView = findViewById(R.id.statusView);
+        Button btnFetch = findViewById(R.id.progressBtn);
+        MyViewModel model = new ViewModelProvider(this).get(MyViewModel.class);
+
+        model.getValue().observe(this, value -> {
+            indicatorBar.setProgress(value);
+            statusView.setText("Статус: " + value);
+        });
+        btnFetch.setOnClickListener(v -> model.execute());
+    }
+}
